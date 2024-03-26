68e344b7c61404118e98a51220c104a18ce14c10
Türkay Ürkmez <turkay@turkayurkmez.com>
2024-03-26 12:20:26 +0300

    talking about pipe (|) in angular

--- projectTracker/src/app/models/mocks/projects.mock.ts
+++ projectTracker/src/app/models/mocks/projects.mock.ts
@@ -8,13 +8,13 @@ export const projects: Project[] = [
         new Task(3, 'Task C',"Task'ın açıklaması",new Date(2024,2,15),1,true),
 
     ],1),
-    new Project(2, 'Project A', 'Project A Açıklama', new Date(2024,2,15),0.10,[
+    new Project(2, 'PROJECT B', 'Project A Açıklama', new Date(2024,2,15),0.10,[
         new Task(1, 'Task A',"Task'ın açıklaması",new Date(2024,2,15),2,false),
         new Task(2, 'Task B',"Task'ın açıklaması",new Date(2024,2,15),2,false),
         new Task(3, 'Task C',"Task'ın açıklaması",new Date(2024,2,15),2,true),
 
     ],2),
-    new Project(2, 'Project A', 'Project A Açıklama', new Date(2024,2,15),0.10,[
+    new Project(2, 'pRoJECt C', 'Project A Açıklama', new Date(2024,2,15),0.10,[
         new Task(4, 'Task A',"Task'ın açıklaması",new Date(2024,2,15),1,false),
         new Task(5, 'Task B',"Task'ın açıklaması",new Date(2024,2,15),1,false),
         new Task(6, 'Task C',"Task'ın açıklaması",new Date(2024,2,15),1,true),
