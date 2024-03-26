927b84c66c3d7b1f7e74dfd0a01b92aa0ee1e032
Türkay Ürkmez <turkay@turkayurkmez.com>
2024-03-26 11:22:45 +0300

    header and department menu init.

--- projectTracker/src/app/departments-menu/departments-menu.component.ts
+++ projectTracker/src/app/departments-menu/departments-menu.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Department } from '../models/department.model';
+import { departments } from '../models/mocks/department.mocks';
 
 @Component({
   selector: 'app-departments-menu',
@@ -6,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './departments-menu.component.css'
 })
 export class DepartmentsMenuComponent {
-
+    departments: Department[] = departments
 }
---
+++ projectTracker/src/app/models/mocks/department.mocks.ts
@@ -0,0 +1,9 @@
+import { Department } from "../department.model";
+
+export const departments: Department[] = [
+    new Department(1, 'Kurumsal Krediler'),
+    new Department(2, 'Bireysel Bankacılık'),
+    new Department(3, 'Ödemeler'),
+    new Department(4, 'Fiyatlama'),
+
+]
