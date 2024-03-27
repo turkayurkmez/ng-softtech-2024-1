71063f8c2748e199105ffef558c5189fd184af74
Türkay Ürkmez <turkay@turkayurkmez.com>
2024-03-27 11:42:17 +0300

    template form in angular

--- projectTracker/src/app/add-department/add-department.component.ts
+++ projectTracker/src/app/add-department/add-department.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Department } from '../models/department.model';
 
 @Component({
   selector: 'app-add-department',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrl: './add-department.component.css'
 })
 export class AddDepartmentComponent {
+
+  formIsInvalid: boolean = false;
+  department: Department = new Department();
+  submitForm(form: NgForm) {
+    if (form.valid) {
+      console.log(form.value);
+
+    }else{
+      console.log('form hatalı!',form.errors);
+      console.log(form.getError('required'));
+      console.log(form.control);
+      this.formIsInvalid = true;
+    }
+  }
 
 }
--- projectTracker/src/app/app-routing.module.ts
+++ projectTracker/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddDepartmentComponent } from './add-department/add-department.component';
+import { ProjectListComponent } from './project-list/project-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:ProjectListComponent},
+  {path:'tumProjeler',component:ProjectListComponent},
+  {path:'departmanEkle', component:AddDepartmentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
